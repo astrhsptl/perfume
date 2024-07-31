b7d7ff03d47646ad7b482436cff565fd029ee2eb
astrhsptl <alexander.kizimenko@mail.ru>
2024-07-31 17:46:07 +0300

    asdasd

--- src/shop/src/widgets/product/payload-container/ui/image-container.tsx
+++ src/shop/src/widgets/product/payload-container/ui/image-container.tsx
@@ -1,11 +1,33 @@
+import { ProductStyle } from '@/shared';
+import Image from 'next/image';
+
 interface ImageContainerProps {}
 
 export const ImageContainer = async ({}: ImageContainerProps) => {
   return (
-    <section>
-      <div>back images</div>
+    <section className={ProductStyle.imageContainer}>
+      <div className={ProductStyle.secondImageContainer}>
+        {[1, 2, 3, 4, 5].map((index) => (
+          <Image
+            key={index}
+            alt='Текущее изображение'
+            src='/photo.png'
+            width={100}
+            height={100}
+            layout='responsive'
+          />
+        ))}
+      </div>
       <div>
-        <div>curremt image</div>
+        <div>
+          <Image
+            alt='Текущее изображение'
+            src='/perfume-skeleton.png'
+            width={400}
+            height={400}
+            layout='responsive'
+          />
+        </div>
         <div>description</div>
       </div>
     </section>
